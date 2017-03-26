8bc8fe45813571b2f1571df812cdfe15e69015c2
CKC\englich <englich@ckc.de>
2017-03-26 16:23:03 +0200

    Fixes AND Todos removed

--- app/src/main/java/eu/englich/payments/ExampleFragment.java
+++ app/src/main/java/eu/englich/payments/ExampleFragment.java
@@ -1,16 +1,13 @@
 package eu.englich.payments;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.RatingBar;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 
@@ -20,12 +17,10 @@ import android.widget.Toast;
  * create an instance of this fragment.
  */
 public class ExampleFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -41,7 +36,6 @@ public class ExampleFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment ExampleFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static ExampleFragment newInstance(String param1, String param2) {
         ExampleFragment fragment = new ExampleFragment();
         Bundle args = new Bundle();
